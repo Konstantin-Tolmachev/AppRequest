9d6d0fec92062875e8bf2a5c6edc3cc46cab542e
Konstantin <kostia132@mail.ru>
2022-01-03 23:17:21 +0500

    1

--- src/main/java/com/practikum/naumen/configs/WebSecurityConfig.java
+++ src/main/java/com/practikum/naumen/configs/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/registration","/registration/**","/admin","/admin/**").permitAll()
+                .antMatchers("/","/registration","/registration/**","/registrationn/**").permitAll()
 //                antMatchers("/","/registration",
 //                        "/staff-request/","/staff-request/**","/staff-status","/staff-filter-request",
 //                        "/admin","/admin/**","/admin-request","/admin-status","/admin-account").permitAll()
